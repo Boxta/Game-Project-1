59a4a5dea5d7c1c54c202eb91f9f785052172639
Born After TV <bornaftertv@gmail.com>
2017-04-10 19:26:35 +1200

    Add Basic Comments

--- Game Project 1/game.cpp
+++ Game Project 1/game.cpp
@@ -8,12 +8,19 @@ using namespace sf;
 
 int main()
 {
+	//Create a window passing in the window size and title bar name
 	RenderWindow window(VideoMode(600, 600), "SFML works!");
+
+	//Create a SFML Circle shape passing in the radius
 	CircleShape shape(300.f);
+
+	//Set the color of the created shape
 	shape.setFillColor(Color::Green);
 
+	//Continue to loop while the window is open
 	while (window.isOpen())
 	{
+		//Check for any events
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -21,8 +28,13 @@ int main()
 				window.close();
 		}
 
+		//Clear the window before drawing
 		window.clear();
+
+		//Draw the shape to the cleared window
 		window.draw(shape);
+
+		//Render the drawn item(s) to the window and display to the user
 		window.display();
 	}
 
